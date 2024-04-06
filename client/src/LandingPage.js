4e7f32af2471e2ef6bc1de682a4fe42f70ef0b72
Ziqing <winifred.wang.2004@gmail.com>
2024-04-06 14:34:52 -0700

    created main container

---
+++ client/src/LandingPage.js
@@ -0,0 +1,13 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+    const Month = "March";
+    const day = 20;
+    return (
+        <div className="mainContainer">
+            
+        </div>
+    );
+}
+
+export default LandingPage;
